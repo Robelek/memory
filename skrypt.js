231937b701270dd5fbd3e97e57569f60b059688c
Robelek <robeleczek@gmail.com>
2022-06-02 20:30:14 +0200

    Nowosci: -nowe obrazki do kart -poprawienie ulozenia kart -dodanie obracania kart

--- skrypt.js
+++ skrypt.js
@@ -3,24 +3,65 @@ let ileKart = 20;
 let naRzad = ileKart/5;
 let kontenerKart=document.querySelector(".kontener-gry");
 
+let sciezka = "images/";
+let symbolSciezka = sciezka+"symbol";
+
+let wszystkieKarty = new Array();
 //
+function losowyNumer()
+{
+    return 1;
+}
 function nowaGra()
 {
+
     while(kontenerKart.lastElementChild)
     {
         kontenerKart.removeChild(kontenerKart.lastElementChild);
     }
   
-        for(let i=0;i<ileKart;i++)
+        for(let i=0;i<ileKart;i+=2)
         {
-            let karta = document.createElement("div");
-            karta.classList.add("karta");
-            
-            kontenerKart.appendChild(karta);
-            let img = document.createElement("img");
-            karta.appendChild(img);
+            let nrSymbolu=losowyNumer();
+            for(j=0;j<2;j++)
+            {
+                let karta = document.createElement("div");
+                karta.classList.add("karta");
+                
+
+                karta.classList.add("symbol"+nrSymbolu);
+                kontenerKart.appendChild(karta);
+
+                karta.addEventListener("click", odwrocKarte);
+
+                let img = document.createElement("img");
+                karta.appendChild(img);
+                img.setAttribute("src", sciezka+"tylKarty.png");
+            }
+           
     
          }
+}
+function odwrocWszystkieKarty()
+{
 
-    
+}
+function odwrocKarte()
+{
+  // console.table(this.classList);
+   // console.log(this.classList.length);
+   let ktorySymbol = 0;
+    for(let i=0;i<this.classList.length;i++)
+    {
+        let nazwaKlasy = this.classList[i];
+        if(nazwaKlasy.includes("symbol"))
+        {
+            //console.log(nazwaKlasy);
+            let dlugosc = nazwaKlasy.length;
+            ktorySymbol = nazwaKlasy[dlugosc-1];
+            console.log(ktorySymbol);
+        }
+    }
+    console.log(this.lastElementChild);
+    this.lastElementChild.setAttribute("src", symbolSciezka+ktorySymbol.toString()+".png");
 }
