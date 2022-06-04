1f14c8679a441f95d5732f888ff064448a767103
Robelek <robeleczek@gmail.com>
2022-06-04 12:27:26 +0200

    Zmiany: -ulozenie kart w rzedy poprawnie

--- skrypt.js
+++ skrypt.js
@@ -1,6 +1,6 @@
 //
 let ileKart = 20;
-let naRzad = ileKart/5;
+let naRzad = ileKart/4;
 let kontenerKart=document.querySelector(".kontener-gry");
 
 let sciezka = "images/";
@@ -18,9 +18,9 @@ let symboli=6;
 let poprzedniaKarta = 0-1;
 //
 let testowyArray = [3, 5, 7, 8, 9];
-console.log(testowyArray[0]);
+////console.log(testowyArray[0]);
 testowyArray.splice(0, 1);
-console.log(testowyArray);
+//console.log(testowyArray);
 
 function losowyNumer(maksimum, minimum)
 {
@@ -34,16 +34,18 @@ function nowaGra()
         kontenerKart.removeChild(kontenerKart.lastElementChild);
     }
   
-        for(let i=0;i<ileKart;i+=2)
+        for(let i=0;i<ileKart;i+=naRzad)
         {
-            let nrSymbolu=losowyNumer(6, 1);
-            for(j=0;j<2;j++)
+            let rzad = document.createElement("div");
+            rzad.classList.add("rzad");
+            kontenerKart.appendChild(rzad);
+            for(j=0;j<naRzad;j++)
             {
                 let karta = document.createElement("div");
                 karta.classList.add("karta");
                 karta.id=(i+j).toString();
                 //karta.classList.add("symbol"+nrSymbolu);
-                kontenerKart.appendChild(karta);
+                rzad.appendChild(karta);
 
                 karta.addEventListener("click", odwrocKarte);
 
@@ -56,15 +58,15 @@ function nowaGra()
            
     
          }
-        console.table(nieUzyteNumery);
+        //console.table(nieUzyteNumery);
         while(nieUzyteNumery.length>0)
         {
             let nrSymbolu = losowyNumer(6, 1);
             for(let i=0;i<2;i++)
             {
                 let ktoraKarta = losowyNumer(0, nieUzyteNumery.length);
-                //console.table(nieUzyteNumery);
-                console.log(nieUzyteNumery[ktoraKarta]);
+                ////console.table(nieUzyteNumery);
+                //console.log(nieUzyteNumery[ktoraKarta]);
                 let karta = document.getElementById(nieUzyteNumery[ktoraKarta].toString());
                 karta.classList.add("symbol"+nrSymbolu);
                 
@@ -80,7 +82,7 @@ function odwrocWszystkieKarty()
     {
         
         let karta = document.getElementById(i);
-        console.log(karta.classList.contains("ok"));
+        //console.log(karta.classList.contains("ok"));
         if(!karta.classList.contains("ok"))
         {
             karta.lastElementChild.setAttribute("src", sciezka+"tylKarty.png");
@@ -92,8 +94,8 @@ function odwrocWszystkieKarty()
 }
 function odwrocKarte()
 {
-  // console.table(this.classList);
-   // console.log(this.classList.length);
+  // ////console.table(this.classList);
+   // ////console.log(this.classList.length);
    if(zepsul)
    {
        zepsul=false;
@@ -105,19 +107,20 @@ function odwrocKarte()
         let nazwaKlasy = this.classList[i];
         if(nazwaKlasy.includes("symbol"))
         {
-            //console.log(nazwaKlasy);
+            //////console.log(nazwaKlasy);
             let dlugosc = nazwaKlasy.length;
             ktorySymbol = nazwaKlasy[dlugosc-1];
-           // console.log(ktorySymbol);
+           // ////console.log(ktorySymbol);
         }
     }
-   // console.log(this.lastElementChild);
+   // ////console.log(this.lastElementChild);
     this.lastElementChild.setAttribute("src", symbolSciezka+ktorySymbol.toString()+".png");
     if(poprzedniSymbol!=0)
     {
         if(poprzedniSymbol==ktorySymbol)
         {
             //trafil dobre karty, czyli usuwamy z je z gry
+            console.log("Trafil dobre karty");
             this.classList.add("ok");
             poprzedniaKarta.classList.add("ok");
             poprzedniSymbol=0;
@@ -125,11 +128,13 @@ function odwrocKarte()
         }
         else
         {
+            console.log("zepsul");
             zepsul=true;
         }
     }
     else
     {
+        console.log("PierwszaKarta");
         poprzedniSymbol=ktorySymbol;
         poprzedniaKarta=this;
     }
